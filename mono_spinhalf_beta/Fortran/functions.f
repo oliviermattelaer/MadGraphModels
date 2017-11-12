2216b0d4efb2cc569eb2e0d7fbf89f97d0d02ec8
asantra <arka@arka-XPS-L521X>
2017-11-12 16:44:23 +0100

    third

--- mono_spinhalf_beta/Fortran/functions.f
+++ mono_spinhalf_beta/Fortran/functions.f
@@ -6,7 +6,7 @@ c Momenta of particles in event
 double complex mymdl_FormFactor(M1):
 
       double complex M1
-      include input.inc ! include all model parameter
+      include '/home/arka/LatestMadGraph5/MG5_aMC_v2_5_4/Pre6/Source/input.inc' ! include all model parameter
       
       mymdl_FormFactor = SQRT(1 - M1**2/(pp(0,2)**2)) ! beta = sqrt(1 - mass^2/energy^2)
       return 
