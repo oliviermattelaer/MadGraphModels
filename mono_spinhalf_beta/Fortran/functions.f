46bf9d9fdb148e7d215297de4f08e926dbef1865
asantra <santra.arka@gmail.com>
2017-11-13 16:33:43 +0100

    Update functions.f

--- mono_spinhalf_beta/Fortran/functions.f
+++ mono_spinhalf_beta/Fortran/functions.f
@@ -7,10 +7,7 @@ c Momenta of particles in event
       
       include 'input.inc' ! include all model parameter
       double complex M1
-<<<<<<< HEAD
-=======
       include 'input.inc' ! include all model parameter
->>>>>>> 4507bc111ff7f87413f4a96c3b60f2a6e927d7f4
       
       mymdl_FormFactor = SQRT(1 - M1**2/(pp(0,2)**2)) ! beta = sqrt(1 - mass^2/energy^2)
       return 
