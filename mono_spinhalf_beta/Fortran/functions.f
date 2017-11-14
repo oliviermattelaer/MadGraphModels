fb488cc3885b3bf99fd9efe6aa2c4ae6ecd90ef8
asantra <arka@arka-XPS-L521X>
2017-11-14 18:36:18 +0100

    seventh

--- mono_spinhalf_beta/Fortran/functions.f
+++ mono_spinhalf_beta/Fortran/functions.f
@@ -1,14 +1,18 @@
-c Momenta of particles in event
-      implicit none
-      include '/home/arka/LatestMadGraph5/MG5_aMC_v2_5_4/Pre6/Source/genps.inc'
-      double precision pp(0:3,max_particles)
-      common/momenta_pp/pp
+c Momenta of particles in event, written by the user
 
-      double complex mymdl_FormFactor(M1)
+      DOUBLE PRECISION FUNCTION mymdl_FormFactor(AAA)
+      IMPLICIT NONE
       
-      include 'input.inc' ! include all model parameter
-      double complex M1
+      INCLUDE '/home/arka/LatestMadGraph5/MG5_aMC_v2_5_4/Pre6/Source/genps.inc'
+      DOUBLE PRECISION pp(0:3,max_particles)
+      COMMON/momenta_pp/pp
+      INCLUDE 'input.inc' ! include all model parameter
       
-      mymdl_FormFactor = SQRT(1 - M1**2/(pp(0,2)**2)) ! beta = sqrt(1 - mass^2/energy^2)
-      return 
-      end
+      DOUBLE PRECISION AAA
+      
+      DOUBLE PRECISION SHAT
+      
+      SHAT = pp(1,3)*pp(1,3)+pp(2,3)*pp(2,3)+pp(3,3)*pp(3,3)
+      
+      mymdl_FormFactor = SQRT(1 - AAA*AAA/(SHAT + AAA*AAA)) !SQRT(1 - AAA**2/(10+AAA**2)) ! beta = sqrt(1 - mass^2/energy^2)
+      END
