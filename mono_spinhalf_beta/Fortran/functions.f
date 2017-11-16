50b321f54d795654ecf7fdcca4de200d75524cd7
asantra <arka@arka-XPS-L521X>
2017-11-16 14:34:07 +0100

    LogFile2

--- mono_spinhalf_beta/Fortran/functions.f
+++ mono_spinhalf_beta/Fortran/functions.f
@@ -6,15 +6,19 @@ c Momenta of particles in event, written by the user
       COMMON/momenta_pp/pp
       INCLUDE 'input.inc' ! include all model parameter
       REAL*8 pp(0:3,max_particles)
-      REAL*8 AAA
-      REAL*8 PSQUARE, OUT
+      REAL*8 AAA, ENERGYSQUARE
+      REAL*8 PSQUARE, OUTVAL
 
       PSQUARE = pp(1,2)*pp(1,2)+pp(2,2)*pp(2,2)+pp(3,2)*pp(3,2)
+      ENERGYSQUARE  = pp(0,2)*pp(0,2)
       
       !mymdl_FormFactor = SQRT(SHAT)/(AAA-AAA+pp(0,2))
       mymdl_FormFactor = SQRT(1.0 - AAA*AAA/(PSQUARE+AAA*AAA))  ! beta = sqrt(1 - mass^2/energy^2)
-      OUT = SQRT(1.0 - AAA*AAA/(PSQUARE+AAA*AAA))
-      PRINT *, 'Form Factor:', OUT
+      OUTVAL = SQRT(1.0 - AAA*AAA/(PSQUARE+AAA*AAA))
+      PRINT *, 'PSQUARE:', PSQUARE
+      PRINT *, 'MASS: ', AAA
+      PRINT *, 'Form Factor:', OUTVAL
+      PRINT *, 'Energy: ', ENERGYSQUARE 
       
       RETURN
       END
